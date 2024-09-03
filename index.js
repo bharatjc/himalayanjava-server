d868d82b1bdea76fe9ddc6b84462846ced98ccc8
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-09-03 22:19:18 +0545

    new ch

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const authRoute = require("./route/auth");
+const forgotPasswordRoute = require("./route/forgotPassword");
 require("./config/database");
 const port = process.env.PORT;
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/", forgotPasswordRoute);
 
 app.use("/api", authRoute);
 
--- route/forgotPassword.js
+++ route/forgotPassword.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const {
+  forgotPassword,
+  resetPassword,
+} = require("../controller/forgotPassword");
 
+router.post("/forgotpassword", forgotPassword);
+router.post("/resetpassword/:id/:token", resetPassword);
+
+module.exports = router;
